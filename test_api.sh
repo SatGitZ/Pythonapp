c7c078160e1d34c6c4ee66548a022a9e23919583
prakash <srijay0311@gmail.com>
2025-08-04 13:59:54 +0530

    updated

--- test_api.sh
+++ test_api.sh
@@ -6,7 +6,7 @@ docker run --rn $CONTAINER_NAME
 NUM1=5
 NUM2=6
 RESULT = ((NUM1 + NUM2))
-EXPECTED = $(curl -s("http://localhost:5000&add/?num1=$NUM1&&num2=$NUM2))
+EXPECTED = $(curl -s"http://localhost:5000"&add/?num1=$NUM1&&num2=$NUM2)
 if ["RESULT"= "EXPECTED"];then
 echo ("success")
 exit 0
